977fe2c97ad649bed7f64ba6f6103c4c8029c533
Raphaël OSTROV <ro@virtualbeings.co>
2023-08-23 15:25:51 +0200

    Version 1.1.10

---
+++ Runtime/Ressources/CommunicationClient.cs
@@ -0,0 +1,257 @@
+// ======================================================================
+// This file contains proprietary technology owned by Virtual Beings SAS.
+// Copyright 2011-2023 Virtual Beings SAS.
+// ======================================================================
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using VirtualBeings.ActiveCognition.Communication;
+using VirtualBeings.Tech.BehaviorComposition;
+using VirtualBeings.Tech.UnityIntegration;
+
+namespace VirtualBeings.Tech.Resources
+{
+    /// <summary>
+    /// This implementation of the ICommunicatonClient is here to showcase a possible way to deal with sounds
+    /// and KuteEngine.
+    /// Sound capabilities were put in here for mostly two reasons:
+    ///  - The ICommunicationClient interface is accessible from the Being and is per-being
+    ///  - The communication client is the entry-point for talking / emoting, which can require to emit sounds. Since
+    ///    this can be "competing" with the vocalizations, it kinda makes sense that it's handled from there.
+    /// Keep in mind that's by no means mandatory.
+    ///
+    /// This communication client is supposed to be attached to the being prefab, and configured (adding audio sources)
+    /// from there.
+    /// </summary>
+    public class CommunicationClient : MonoBehaviour, ICommunicationClient
+    {
+        [SerializeField]
+        protected AudioSource _faceAudioSource;
+
+        [SerializeField]
+        protected AudioSource _bodyAudioSource;
+
+        public virtual void Init(Being being)
+        {
+            Debug.Assert(_faceAudioSource != null);
+
+            if (_faceAudioSource == null)
+            {
+                throw new Exception("Primary audio source cannot be null");
+            }
+
+            _faceAudioSource.loop        = false;
+            _faceAudioSource.playOnAwake = false;
+            _faceAudioSource.mute        = false;
+
+            if (_bodyAudioSource != null)
+            {
+                _bodyAudioSource.loop        = false;
+                _bodyAudioSource.playOnAwake = false;
+                _bodyAudioSource.mute        = false;
+            }
+        }
+
+        // ------------------------------
+        // Sound emitting functions
+        //
+
+        /// <summary>
+        /// A vocalization will be requested in the near future, prepare for it.
+        /// We basically just stop playing any looping audio here.
+        /// </summary>
+        public virtual void PrepareForVocalization()
+        {
+            if (_faceAudioSource.loop)
+            {
+                _faceAudioSource.loop = false;
+                _faceCoroutine = StartCoroutine(FadeOutClip(_faceAudioSource, 0.2f));
+            }
+        }
+
+        /// <summary>
+        /// Effectively emit the requested vocalization.
+        /// </summary>
+        /// <param name="loop"></param>
+        /// <param name="clip">Can happen to be null</param>
+        /// <param name="volume"></param>
+        /// <param name="pitch"></param>
+        public virtual void EmitVocalization(bool loop, AudioClip clip, float volume, float pitch)
+        {
+            if (clip == null)
+            {
+                return;
+            }
+
+            if (_faceCoroutine != null)
+            {
+                StopCoroutine(_faceCoroutine);
+            }
+            _faceCoroutine = null;
+
+            _faceAudioSource.loop   = loop;
+            _faceAudioSource.clip   = clip;
+            _faceAudioSource.volume = loop ? 0f : volume;
+            _faceAudioSource.pitch  = pitch;
+            _faceAudioSource.Play();
+
+            if (loop)
+            {
+                _faceCoroutine = StartCoroutine(FadeInClip(_faceAudioSource, 0.5f, volume));
+            }
+        }
+
+        /// <summary>
+        /// A body sound will be requested in the near future, prepare for it.
+        /// We basically just stop playing any looping audio here.
+        /// </summary>
+        public virtual void PrepareForBodySound()
+        {
+            if (_bodyAudioSource != null && _bodyAudioSource.loop)
+            {
+                _bodyAudioSource.loop = false;
+                _bodyCoroutine = StartCoroutine(FadeOutClip(_bodyAudioSource, 0.2f));
+            }
+        }
+
+        /// <summary>
+        /// Effectively emit the required body sound.
+        /// </summary>
+        /// <param name="loop"></param>
+        /// <param name="clip">Can happen to be null</param>
+        /// <param name="volume"></param>
+        /// <param name="pitch"></param>
+        public virtual void EmitBodySound(bool loop, AudioClip clip, float volume, float pitch)
+        {
+            if (clip == null)
+            {
+                return;
+            }
+
+            if (_bodyAudioSource == null)
+            {
+                return;
+            }
+
+            if (_bodyCoroutine != null)
+            {
+                StopCoroutine(_bodyCoroutine);
+            }
+            _bodyCoroutine = null;
+
+            _bodyAudioSource.loop   = loop;
+            _bodyAudioSource.clip   = clip;
+            _bodyAudioSource.volume = loop ? 0f : volume;
+            _bodyAudioSource.pitch  = pitch;
+            _bodyAudioSource.Play();
+
+            if (loop)
+            {
+                _bodyCoroutine = StartCoroutine(FadeInClip(_bodyAudioSource, 0.5f, volume));
+            }
+        }
+
+        /// <summary>
+        /// Request a fade out for the given audio source. Will stop looping the currently playing sound.
+        /// </summary>
+        /// <param name="soundSource"></param>
+        /// <param name="fadeoutDuration"></param>
+        public virtual void FadeOutSoundSource(SoundSource soundSource, float fadeoutDuration)
+        {
+            switch (soundSource)
+            {
+                case SoundSource.Body:
+                    if (_bodyAudioSource != null)
+                    {
+                        _bodyAudioSource.loop = false;
+                        _bodyCoroutine        = StartCoroutine(FadeOutClip(_faceAudioSource, fadeoutDuration));
+                    }
+                    break;
+
+                case SoundSource.Face:
+                    _faceAudioSource.loop = false;
+                    _faceCoroutine = StartCoroutine(FadeOutClip(_faceAudioSource, fadeoutDuration));
+                    break;
+            }
+        }
+
+        // ------------------------------
+        // Communication client interface
+        //
+
+        public virtual void DoTextDisplay(string text, float duration)
+        {
+        }
+
+        public virtual void DoEmoteDisplay(int id, float duration)
+        {
+        }
+        public virtual void DoSpeech(string text, float animationIntensity01, float volume01, float speed01, float pitch)
+        {
+        }
+
+        public virtual void TerminateSpeech()
+        {
+        }
+
+        public virtual void CreateChatCompletion(List<ChatMessage> messages, Action<ChatMessage> OnComplete)
+        {
+        }
+
+        public virtual void TerminateChatCompletion()
+        {
+        }
+
+        public virtual bool IsSpeaking    => _isSpeaking;
+        public virtual bool IsSpeechReady => false;
+        public virtual float SpeechDuration => 0f;
+
+        // ------------------------------
+        // Sound emitting helpers
+        //
+
+        // https://johnleonardfrench.com/how-to-fade-audio-in-unity-i-tested-every-method-this-ones-the-best/
+        // First method
+        protected static IEnumerator FadeInClip(
+            AudioSource source,
+            float duration,
+            float targetVolume
+        )
+        {
+            float currentTime = 0f;
+            float start       = source.volume;
+            while (currentTime < duration)
+            {
+                currentTime   += Time.deltaTime;
+                source.volume =  Mathf.Lerp(start, targetVolume, currentTime / duration);
+                yield return null;
+            }
+        }
+
+        protected IEnumerator FadeOutClip(AudioSource source, float duration)
+        {
+            float startTime   = Time.time;
+            float startVolume = Math.Max(.01f, source.volume);
+
+            while (source.volume > 0f)
+            {
+                source.volume = startVolume - (Time.time - startTime) / duration * startVolume;
+                yield return null;
+            }
+
+            source.Stop();
+        }
+
+        // ------------------------------
+        // Private fields
+        //
+
+        protected bool _isSpeaking;
+
+        protected Coroutine _faceCoroutine;
+        protected Coroutine _bodyCoroutine;
+    }
+}
---
+++ Runtime/Ressources/SoundManager.cs
@@ -0,0 +1,311 @@
+﻿// ======================================================================
+// This file contains proprietary technology owned by Virtual Beings SAS.
+// Copyright 2011-2023 Virtual Beings SAS.
+// ======================================================================
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VirtualBeings.Tech.BehaviorComposition;
+using VirtualBeings.Tech.UnityIntegration;
+using VirtualBeings.Tech.Utils;
+
+namespace VirtualBeings.Tech.Resources
+{
+    /// <summary>
+    /// This sound manager is intended as a starting point / example of how to manage the sound interactions
+    /// with beings. For the sake of example, this is expecting a use with the CommunicationClient (same folder), which
+    /// is also responsible for emitting speech (see CommunicationClient for more info).
+    /// </summary>
+    public class SoundManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Settings _settings;
+
+        /// <summary>
+        /// Register to the sound world events we want to be able to catch, and initialize our shopkeeping lists.
+        /// </summary>
+        public void Start()
+        {
+            _worldEvents = Container.Instance.WorldEvents;
+
+            _worldEvents.AddListener<WorldEvent_BeingSoundRequest>(OnSoundRequestEvent);
+            _worldEvents.AddListener<WorldEvent_BeingSoundRequestViaAnimCallback>(OnSoundRequestViaAnimCallbackEvent);
+            _worldEvents.AddListener<WorldAnimEvent_BeingSoundRequest>(OnSoundRequestViaAnimEvent);
+        }
+
+        /// <summary>
+        /// On update, we check if any ongoing looped sound needs to terminate, and ask the communication client
+        /// to do so if required by KuteEngine.
+        /// </summary>
+        private void Update()
+        {
+            for (int i = 0; i < _listOngoingSoundEvents.Count; i++)
+            {
+                OngoingSoundTracker soundEvent = _listOngoingSoundEvents[i];
+                if (soundEvent.Being == null || soundEvent.Being.gameObject == null)
+                {
+                    _listOngoingSoundEvents.RemoveAt(i--);
+                }
+                else
+                {
+                    bool shouldTerminate = soundEvent.TerminationPredicate();
+
+                    if (shouldTerminate)
+                    {
+                        soundEvent.CommunicationClient.FadeOutSoundSource(
+                            soundEvent.SoundSource,
+                            soundEvent.FadeoutDuration
+                        );
+
+                        _listOngoingSoundEvents.RemoveAt(i--);
+                    }
+                }
+            }
+        }
+
+        // ------------------------------
+        // Audio event handlers
+        //
+
+        /// <summary>
+        /// Called when we receive a direct sound request event.
+        /// We start by clearing the OnGoingSoundTracker list, and then we get the CommunicationClient from the being.
+        /// Then, we tell the client to start playing the sound, depending on the source. Finally, we raise a world event
+        /// to notify the beings that something audio related is happening.
+        /// If there's a termination predicate associated with the sound request, we start tracking it.
+        /// </summary>
+        /// <param name="soundRequest"></param>
+        private void OnSoundRequestEvent(WorldEvent_BeingSoundRequest soundRequest)
+        {
+            OngoingSoundTracker.PurgeBeingFromList(soundRequest.Being, _listOngoingSoundEvents);
+
+            CommunicationClient client = soundRequest.Being.CommunicationClient as CommunicationClient;
+
+            if (client != null)
+            {
+                switch (soundRequest.SoundSource)
+                {
+                    case SoundSource.Body:
+                        client.PrepareForBodySound();
+                        client.EmitBodySound(
+                            soundRequest.Loop,
+                            soundRequest.Clip,
+                            soundRequest.Volume01,
+                            soundRequest.Pitch
+                        );
+                        break;
+
+                    case SoundSource.Face:
+                        client.PrepareForVocalization();
+                        client.EmitVocalization(
+                            soundRequest.Loop,
+                            soundRequest.Clip,
+                            soundRequest.Volume01,
+                            soundRequest.Pitch
+                        );
+                        break;
+                }
+
+                _worldEvents.Raise(
+                    new WorldEvent_Sound(
+                        soundRequest.Type,
+                        soundRequest.Poignancy01,
+                        soundRequest.Being,
+                        soundRequest.Being
+                    )
+                );
+            }
+
+            if (soundRequest.TerminationPredicate != null)
+            {
+                _listOngoingSoundEvents.Add(
+                    new OngoingSoundTracker(
+                        soundRequest.Being,
+                        client,
+                        soundRequest.SoundSource,
+                        soundRequest.FadeoutDuration,
+                        soundRequest.TerminationPredicate
+                    )
+                );
+            }
+        }
+
+        /// <summary>
+        /// Called when we receive a sound request via animation callback event.
+        /// In that case, we only do two things. We tell the communication client some sound will happen on the
+        /// given sound source, then we just store the sound request, to be processed when we effectively receive
+        /// the anim event.
+        /// </summary>
+        /// <param name="soundRequest"></param>
+        private void OnSoundRequestViaAnimCallbackEvent(WorldEvent_BeingSoundRequestViaAnimCallback soundRequest)
+        {
+            CommunicationClient communicationClient = soundRequest.Being.CommunicationClient as CommunicationClient;
+
+            if (communicationClient)
+            {
+                if (soundRequest.SoundSource == SoundSource.Body)
+                {
+                    communicationClient.PrepareForBodySound();
+                }
+                else if (soundRequest.SoundSource == SoundSource.Face)
+                {
+                    communicationClient.PrepareForVocalization();
+                }
+
+                QueueSoundRequest(soundRequest);
+            }
+        }
+
+        /// <summary>
+        /// When the animation effectively triggers the callback, we receive this event.
+        /// In that case, we look out for the anim event info, given the being and the sound source.
+        /// If we properly find it - and it's still valid, we play it, as done for a standard sound request.
+        /// </summary>
+        /// <param name="soundRequestViaAnim"></param>
+        private void OnSoundRequestViaAnimEvent(WorldAnimEvent_BeingSoundRequest soundRequestViaAnim)
+        {
+            if (TryGetAndClearQueuedSoundRequest(
+                    soundRequestViaAnim,
+                    out WorldEvent_BeingSoundRequestViaAnimCallback soundRequest
+                ))
+            {
+                CommunicationClient communicationClient = soundRequestViaAnim.Being.CommunicationClient as CommunicationClient;
+
+                if (soundRequest.Being.TopLevelParent != null && communicationClient != null &&
+                    soundRequest.ValidUntil > Time.time)
+                {
+                    OngoingSoundTracker.PurgeBeingFromList(
+                        soundRequest.Being,
+                        _listOngoingSoundEvents
+                    );
+
+                    if (soundRequest.SoundSource == SoundSource.Body)
+                    {
+                        communicationClient.EmitBodySound(
+                            soundRequest.Loop,
+                            soundRequest.Clip,
+                            soundRequest.Volume01,
+                            soundRequest.Pitch
+                        );
+                    }
+                    else if (soundRequest.SoundSource == SoundSource.Face)
+                    {
+                        communicationClient.EmitVocalization(
+                            soundRequest.Loop,
+                            soundRequest.Clip,
+                            soundRequest.Volume01,
+                            soundRequest.Pitch
+                        );
+                    }
+
+                    _worldEvents.Raise(
+                        new WorldEvent_Sound(
+                            soundRequest.Type,
+                            soundRequest.Poignancy01,
+                            soundRequest.Being,
+                            soundRequest.Being
+                        )
+                    );
+                }
+            }
+        }
+
+        // ------------------------------
+        // Variables
+
+        /// <summary>
+        /// The KuteEngine event manager, which emits the sound events that we need to subscribe to.
+        /// </summary>
+        private EventManager _worldEvents;
+        private readonly List<OngoingSoundTracker> _listOngoingSoundEvents = new(4);
+        private readonly Dictionary<Being, Dictionary<SoundSource, WorldEvent_BeingSoundRequestViaAnimCallback>>
+            _dictScheduledSoundRequestViaCallbackEvents = new(4);
+
+        void QueueSoundRequest(WorldEvent_BeingSoundRequestViaAnimCallback soundRequest)
+        {
+            if (!_dictScheduledSoundRequestViaCallbackEvents.TryGetValue(
+                    soundRequest.Being,
+                    out Dictionary<SoundSource, WorldEvent_BeingSoundRequestViaAnimCallback> soundRequests
+                ))
+            {
+                soundRequests = new Dictionary<SoundSource, WorldEvent_BeingSoundRequestViaAnimCallback>(2);
+                _dictScheduledSoundRequestViaCallbackEvents[soundRequest.Being] = soundRequests;
+            }
+
+            soundRequests[soundRequest.SoundSource] = soundRequest;
+        }
+
+        bool TryGetAndClearQueuedSoundRequest(
+            WorldAnimEvent_BeingSoundRequest soundRequest,
+            out WorldEvent_BeingSoundRequestViaAnimCallback result
+        )
+        {
+            result = null;
+
+            if (!_dictScheduledSoundRequestViaCallbackEvents.TryGetValue(
+                    soundRequest.Being,
+                    out Dictionary<SoundSource, WorldEvent_BeingSoundRequestViaAnimCallback> soundRequests
+                ))
+            {
+                return false;
+            }
+
+            if (!soundRequests.TryGetValue(soundRequest.SoundSource, out result))
+            {
+                return false;
+            }
+
+            soundRequests.Remove(soundRequest.SoundSource);
+
+            return true;
+        }
+
+        // ------------------------------
+        // Classes for internal use
+
+        private class OngoingSoundTracker
+        {
+            public Being               Being;
+            public CommunicationClient CommunicationClient;
+            public SoundSource         SoundSource;
+            public float               FadeoutDuration;
+            public Func<bool>          TerminationPredicate;
+
+            public OngoingSoundTracker(
+                Being being,
+                CommunicationClient client,
+                SoundSource soundSource,
+                float fadeoutDuration,
+                Func<bool> terminationPredicate
+            )
+            {
+                Being                = being;
+                CommunicationClient  = client;
+                SoundSource          = soundSource;
+                FadeoutDuration      = fadeoutDuration;
+                TerminationPredicate = terminationPredicate;
+            }
+
+            public static void PurgeBeingFromList(Being being, List<OngoingSoundTracker> list)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (being == list[i].Being)
+                    {
+                        list.RemoveAt(i--);
+                    }
+                }
+            }
+        }
+
+        [Serializable]
+        public class Settings
+        {
+            public float DefaultFadeOutDuration = .3f;
+            public float AmbientFadeOutDuration = 5f;
+            public float DefaultFadeInDuration  = 1f;
+        }
+    }
+}
